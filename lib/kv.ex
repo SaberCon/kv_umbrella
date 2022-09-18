1446301cddd9112acb9a6589c8701b01288e5466
SaberCon <894876785@qq.com>
2022-09-18 16:53:54 +0800

    Learn Supervisor and Application

--- lib/kv.ex
+++ lib/kv.ex
@@ -3,6 +3,15 @@ defmodule KV do
   Documentation for `KV`.
   """
 
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    # Although we don't use the supervisor name below directly,
+    # it can be useful when debugging or introspecting the system.
+    KV.Supervisor.start_link(name: KV.Supervisor)
+  end
+
   @doc """
   Hello world.
 
